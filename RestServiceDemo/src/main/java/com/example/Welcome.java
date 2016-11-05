c9635f0e32ff1c7b96a1ff5c3a79927518a0e6b1
sadiq <sb.itsolutions7080@gmail.com>
2016-11-05 23:14:27 +0530

    FirstCommit

---
+++ RestServiceDemo/src/main/java/com/example/Welcome.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+ @RequestMapping("/welcome")
+	public  @ResponseBody String welcome() {
+		return "Welcome";
+		
+		
+	}
+	
+
+}
